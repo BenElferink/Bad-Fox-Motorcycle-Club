7fe8bcef11b3315c9f8c7e631bb297be633a59ab
Ben Elferink <ben.elferink@icloud.com>
2022-04-11 17:12:58 +0300

    refactored team section

---
+++ client/src/components/Team/Card.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { Avatar, IconButton } from '@mui/material'
+import Twitter from '../../icons/Twitter'
+import LinkedIn from '../../icons/LinkedIn'
+import Instagram from '../../icons/Instagram'
+import { INSTAGRAM, LINKEDIN, TWITTER } from '../../constants'
+import styles from './Team.module.css'
+
+export default function Card({ item }) {
+  const [deg, setDeg] = useState(0)
+  const borderPx = '10px'
+
+  const doFlip = () => {
+    setDeg(180)
+    setTimeout(() => {
+      setDeg(0)
+    }, 300)
+  }
+
+  const clickSocial = (url) => {
+    window.open(url, '_blank')
+  }
+
+  return (
+    <div className={styles.card} style={{ borderRadius: borderPx, transform: `rotateY(${deg}deg)` }} onMouseEnter={doFlip}>
+      <Avatar src={item.profileSrc} className={styles.avatar} />
+
+      <div>
+        <h4>{item.name}</h4>
+        <h6>{item.title}</h6>
+        <p>{item.description}</p>
+      </div>
+
+      <div className={styles.socials}>
+        {item.socials.map((social) => (
+          <IconButton key={`${item.name}-${social.type}`} onClick={() => clickSocial(social.url)}>
+            {(() => {
+              switch (social.type) {
+                case TWITTER:
+                  return <Twitter />
+                case LINKEDIN:
+                  return <LinkedIn />
+                case INSTAGRAM:
+                  return <Instagram />
+                default:
+                  return null
+              }
+            })()}
+          </IconButton>
+        ))}
+      </div>
+    </div>
+  )
+}
--- client/src/components/Team/index.js
+++ client/src/components/Team/index.js
@@ -1,98 +1,14 @@
 import { forwardRef } from 'react'
-import { Avatar, IconButton } from '@mui/material'
-import Twitter from '../../icons/Twitter'
-import LinkedIn from '../../icons/LinkedIn'
-import Instagram from '../../icons/Instagram'
-import { INSTAGRAM, LINKEDIN, TEAM, TWITTER } from '../../constants'
+import { TEAM } from '../../constants'
+import Card from './Card'
+import team from './team.json'
 import styles from './Team.module.css'
 
-const team = [
-  {
-    profileSrc: '/images/team/ben.jpeg',
-    name: 'Ben Elferink',
-    title: 'Lead Developer / Community Manager',
-    description:
-      "I'm a Fullstack Developer with 3 years of experience, and have been involved in the CNFT space for almost a year now. I have helped build several communities at their early stages.",
-    socials: [
-      {
-        type: LINKEDIN,
-        url: 'https://www.linkedin.com/in/ben-elferink-37ba251b9',
-      },
-      {
-        type: TWITTER,
-        url: 'https://twitter.com/BenElferink',
-      },
-    ],
-  },
-  {
-    profileSrc: '/images/team/david.jpg',
-    name: 'David Minkov',
-    title: 'Lead Artist',
-    description:
-      'I have 2 years of experience in graphic design, and am an artist from a very young age. I just dived into the CNFT space, and am very excited to work on Bad Fox MC!',
-    socials: [
-      {
-        type: LINKEDIN,
-        url: 'https://www.linkedin.com/in/david-minkov-50187620a',
-      },
-      {
-        type: INSTAGRAM,
-        url: 'https://instagram.com/david_minkov',
-      },
-    ],
-  },
-  {
-    profileSrc: '/images/team/chris.jpg',
-    name: 'Christian Mitrev',
-    title: 'Head Founder / Artist',
-    description:
-      "I'm an artist with 2 years of experience, and am a stock/crypto trader with 3 years of experience. I formed the team and initial idea behind Bad Fox MC!",
-    socials: [
-      // https://www.facebook.com/chris.mitrav
-      {
-        type: INSTAGRAM,
-        url: 'https://instagram.com/m__chris',
-      },
-    ],
-  },
-]
-
 const Team = forwardRef((props, ref) => {
-  const clickSocial = (url) => {
-    window.open(url, '_blank')
-  }
-
   return (
     <div ref={ref} id={TEAM} className={styles.root}>
-      {team.map((obj) => (
-        <div key={obj.name} className={styles.card}>
-          <Avatar src={obj.profileSrc} className={styles.avatar} />
-
-          <div>
-            <h4>{obj.name}</h4>
-            <h6>{obj.title}</h6>
-            <p>{obj.description}</p>
-          </div>
-
-          <div className={styles.socials}>
-            {obj.socials.map((socObj) => (
-              <IconButton key={`${obj.name}-${socObj.type}`} onClick={() => clickSocial(socObj.url)}>
-                {(() => {
-                  switch (socObj.type) {
-                    case TWITTER:
-                      return <Twitter />
-                    case LINKEDIN:
-                      return <LinkedIn />
-                    case INSTAGRAM:
-                      return <Instagram />
-                    default:
-                      return null
-                  }
-                })()}
-              </IconButton>
-            ))}
-          </div>
-        </div>
+      {team.map((item) => (
+        <Card key={item.name} item={item} />
       ))}
     </div>
   )
