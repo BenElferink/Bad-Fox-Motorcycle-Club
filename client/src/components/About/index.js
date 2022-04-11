aa72ff5c6dde4ed90eddc35ec56c24905051b656
Ben Elferink <ben.elferink@icloud.com>
2022-04-11 17:20:48 +0300

    fix about text

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -5,12 +5,12 @@ export default function About() {
     <section className={styles.root}>
       <h2>About Bad Fox MC</h2>
       <p>
-        Bad Fox Motorcycle Club is a <strong>community driven</strong> project, coming from the <strong>best projects on Cardano</strong>, with close
-        groups of friends in the CNFT space being the <strong>core of the community</strong>.
+        Bad Fox Motorcycle Club is a <strong>community driven</strong> NFT project, coming from the <strong>best projects on Cardano</strong>, with
+        close groups of friends in the CNFT space being the <strong>core of the community</strong>.
       </p>
       <p>
-        In terms of utility, BFMC will be <strong>rewarding all holders passively</strong> with ADA, and will have <strong>a burn event</strong>{' '}
-        allowing participants to get <strong>lifetime access to metaverse content and assets</strong>.
+        In terms of utility, BFMC will be <strong>rewarding all holders passively</strong> with ADA, and will (later on) have{' '}
+        <strong>a burn event</strong> allowing participants to get <strong>lifetime access to metaverse content and assets</strong>.
       </p>
     </section>
   )
