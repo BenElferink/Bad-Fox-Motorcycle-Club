9dd69e336820d1b9fa1def0e1b97018e95adbc6b
Ben Elferink <ben.elferink@icloud.com>
2022-04-12 20:03:35 +0300

    renamed json file

--- client/src/components/Team/index.js
+++ client/src/components/Team/index.js
@@ -1,13 +1,13 @@
 import { forwardRef } from 'react'
 import { TEAM } from '../../constants'
 import Card from './Card'
-import team from './team.json'
+import data from './data.json'
 import styles from './Team.module.css'
 
 const Team = forwardRef((props, ref) => {
   return (
     <div ref={ref} id={TEAM} className={styles.root}>
-      {team.map((item) => (
+      {data.map((item) => (
         <Card key={item.name} item={item} />
       ))}
     </div>
