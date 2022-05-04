4296d21eea8611af551edaabf4b87e138b9a329a
Ben Elferink <ben.elferink@icloud.com>
2022-05-04 15:09:43 +0300

    fix: prod redirect uri

--- constants/discord.js
+++ constants/discord.js
@@ -4,7 +4,7 @@ module.exports = {
   DISCORD_REDIRECT_URL_REGISTER:
     process.env.NODE_ENV === 'development'
       ? 'https://discord.com/api/oauth2/authorize?client_id=967368512941203466&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fregister%2Fredirect&response_type=token&scope=identify'
-      : 'https://discord.com/api/oauth2/authorize?client_id=967368512941203466&redirect_uri=https%3A%2F%2Fbadfoxmc.com%2Fregister%2Fredirect&response_type=code&scope=identify',
+      : 'https://discord.com/api/oauth2/authorize?client_id=967368512941203466&redirect_uri=https%3A%2F%2Fbadfoxmc.com%2Fregister%2Fredirect&response_type=token&scope=identify',
   DISCORD_ROLE_ID_OG: '957270960065609798',
   DISCORD_ROLE_ID_WL: '957270836220407818',
   DISCORD_ROLE_ID_PUBLIC_RESERVE: '969214730172514354',
