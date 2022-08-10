e18012e475e5d03342440a885e13d1ec8d79c662
Ben Elferink <ben.elferink@icloud.com>
2022-08-10 18:41:21 +0300

    ipfs no longer supports public gateway, fixed url formatter

--- functions/formatters/formatIpfsImageUrl.js
+++ functions/formatters/formatIpfsImageUrl.js
@@ -1,6 +1,6 @@
 const formatIpfsImageUrl = (ipfsUri) => {
   // return `https://alwaysinvert.mypinata.cloud/ipfs/${ipfsUri.replace('ipfs://', '')}`
-  return `https://ipfs.io/ipfs/${ipfsUri.replace('ipfs://', '')}`
+  return `https://ipfs.blockfrost.dev/ipfs/${ipfsUri.replace('ipfs://', '')}`
 }
 
 module.exports = formatIpfsImageUrl
