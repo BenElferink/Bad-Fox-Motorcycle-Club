14fd0738eee6905893f9a902920b01c229b63a2b
Ben Elferink <ben.elferink@icloud.com>
2022-08-19 01:11:08 +0300

    fox cron job

--- _bots/runCronJobs.js
+++ _bots/runCronJobs.js
@@ -1,20 +1,23 @@
+require('dotenv').config()
 const cron = require('node-cron')
 const { default: axios } = require('axios')
 const { ADMIN_CODE } = require('../constants/api-keys')
 const { FOX_POLICY_ID } = require('../constants/policy-ids')
+
+const URI = 'https://badfoxmc.com/api/snapshot'
 
 const runCronJob = async () => {
   console.log('running cron job')
   const config = { headers: { admin_code: ADMIN_CODE } }
 
   try {
-    await axios.head(`/api/snapshot/floor-prices/${FOX_POLICY_ID}`, config)
+    await axios.head(`${URI}/floor-prices/${FOX_POLICY_ID}`, config)
   } catch (error) {
     console.error(error)
   }
 
   try {
-    await axios.head(`/api/snapshot/holders/${FOX_POLICY_ID}`, config)
+    await axios.head(`${URI}/holders/${FOX_POLICY_ID}`, config)
   } catch (error) {
     console.error(error)
   }
@@ -26,3 +29,5 @@ cron.schedule('0 0 * * *', runCronJob, {
   scheduled: true,
   timezone: 'Asia/Jerusalem',
 })
+
+runCronJob()
