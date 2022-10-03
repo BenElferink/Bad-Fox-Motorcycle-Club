a765e36412d755aec0d0e42d5f60a1731144dade
Ben Elferink <ben.elferink@icloud.com>
2022-10-03 15:45:41 +0300

    updated about section

--- components/About/index.js
+++ components/About/index.js
@@ -4,15 +4,10 @@ export default function About() {
   return (
     <section className={styles.root}>
       <h2>About Bad Fox MC</h2>
-      <p>Bad Fox Motorcycle Club is a NFT project on the Cardano Blockchain.</p>
       <p>
-        We're focused on rewarding 100% of holders. There will be no raffle to top rare ranks, or any of that
-        bullsh*t. You hold, you earn, simple as that.
-      </p>
-      <p>
-        At the beginning of our roadmap, holders will be earning ADA through royalty distributions, and
-        through other events. Later on we'll have a burn event allowing participants to get lifetime access to metaverse
-        content and assets through airdrops.
+        Bad Fox Motorcycle Club is a large collective of NFT fans who are working to innovate on what is possible
+        with a PFP project, currently planning diverse forms of fund redistribution to the community and
+        integrations into various metaverse projects such as Cornucopias.
       </p>
     </section>
   )
