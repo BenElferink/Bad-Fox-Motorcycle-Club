fe3dedde213e1426863cb79a15c2174245cc0add
Ben Elferink <ben.elferink@icloud.com>
2022-10-23 18:58:24 +0300

    remove wallet not in use

--- constants/addresses.js
+++ constants/addresses.js
@@ -2,8 +2,6 @@ const BAD_FOX_WALLET =
   'addr1q9p9yq4lz834729chxsdwa7utfp5wr754zkn6hltxz42m594guty04nldwlxnhw8xcgd5pndaaqzzu5qzyvnc8tlgdsqtazkyh'
 const BAD_FOX_ROYALTY_WALLET =
   'addr1qyv7wgxd4fjvp9jxr2v6tdpygmjxwatesaemvassemq6jq2rqhw6rvndlmdnp0y7mwvaux4v2wpz5rusyy8c636az70sjxtwe6'
-const BAD_MOTORCYCLE_ROYALTY_WALLET =
-  'addr1qy3lxlrp726amewszc3rcs44h8rmm33vwj5ras8ya0g5t2rldmc2xmvksm8ec5z6w9rwzy5pnguk5dldfg43jalywges0jmdm5'
 
 const JPG_STORE_WALLET =
   'addr1zxj47sy4qxlktqzmkrw8dahe46gtv8seakrshsqz26qnvzypw288a4x0xf8pxgcntelxmyclq83s0ykeehchz2wtspksr3q9nx'
@@ -16,11 +14,9 @@ module.exports = {
   CNFT_IO_WALLET,
   EPOCH_ART_WALLET,
   BAD_FOX_ROYALTY_WALLET,
-  BAD_MOTORCYCLE_ROYALTY_WALLET,
   EXCLUDE_ADDRESSES: [
     BAD_FOX_WALLET,
     BAD_FOX_ROYALTY_WALLET,
-    BAD_MOTORCYCLE_ROYALTY_WALLET,
     JPG_STORE_WALLET,
     CNFT_IO_WALLET,
     EPOCH_ART_WALLET,
