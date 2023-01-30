42850c0b12f84a3f26de0e15bb5ca90bbbeb63e8
Ben Elferink <ben.elferink@icloud.com>
2023-01-30 16:48:51 +0200

    enable U.E. puzzle

--- components/PartnerProject.tsx
+++ components/PartnerProject.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-// import { Fragment, useState } from 'react'
+import { useState } from 'react'
 
 export interface PartnerProjectProps {
   name: string
@@ -11,36 +11,37 @@ export interface PartnerProjectProps {
 
 const PartnerProject = (props: PartnerProjectProps) => {
   const { name, url, logoUrl, className } = props
-  // const [clickCount, setClickCount] = useState(0)
-  // const [showWord, setShowWord] = useState(false)
+  const [clickCount, setClickCount] = useState(0)
+  const [showWord, setShowWord] = useState(false)
 
-  // if (name === 'Unbounded Earth') {
-  //   return (
-  //     <button
-  //       type='button'
-  //       onClick={() => {
-  //         if (clickCount < 69) {
-  //           setClickCount((prev) => prev + 1)
-  //         } else {
-  //           setShowWord(true)
-  //         }
-  //       }}
-  //       className={'w-20 h-12 m-5 flex flex-col items-center justify-center relative ' + className}
-  //     >
-  //       <Image src={logoUrl} alt={name} fill sizes='5rem' className='object-contain' />
-  //       <h6 className='absolute -bottom-7 text-xs whitespace-nowrap'>
-  //         {name}
-  //         {clickCount ? ` (${clickCount})` : null}
-  //         {showWord ? (
-  //           <Fragment>
-  //             <br />
-  //             <u>Secret</u>: preparation
-  //           </Fragment>
-  //         ) : null}
-  //       </h6>
-  //     </button>
-  //   )
-  // }
+  if (name === 'Unbounded Earth') {
+    return (
+      <button
+        type='button'
+        onClick={() => {
+          if (clickCount < 69) {
+            setClickCount((prev) => prev + 1)
+          } else if (!showWord) {
+            setShowWord(true)
+          } else if (clickCount >= 69 && showWord) {
+            window.open(url, '_blank', 'noopener')
+          }
+        }}
+        className={'w-20 h-10 my-6 mx-6 flex flex-col items-center justify-center relative ' + className}
+      >
+        <Image src={logoUrl} alt={name} fill sizes='5rem' className='object-contain' />
+        <h6 className='absolute -bottom-7 text-xs whitespace-nowrap'>
+          {name}
+          {clickCount ? ` (${clickCount})` : null}
+          {showWord ? (
+            <span className='absolute -bottom-4 left-0'>
+              <u>Secret</u>: preparation
+            </span>
+          ) : null}
+        </h6>
+      </button>
+    )
+  }
 
   return (
     <Link
