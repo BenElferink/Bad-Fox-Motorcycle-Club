b00fcf09437e3621dfcae1c326d18a1e5532494f
Ben Elferink <ben.elferink@icloud.com>
2023-02-04 16:11:24 +0200

    fix ipfs slow rendering

--- functions/formatters/formatIpfsImageUrl.ts
+++ functions/formatters/formatIpfsImageUrl.ts
@@ -2,8 +2,8 @@ const formatIpfsImageUrl = (ipfsUri: string, hasRank?: boolean) => {
   if (hasRank) {
     return ipfsUri.replace('ipfs://', 'https://images.cnft.tools/ipfs/')
   } else {
-    return ipfsUri.replace('ipfs://', 'https://ipfs.jpgstoreapis.com/')
-    // return ipfsUri.replace('ipfs://', 'https://ipfs.blockfrost.dev/ipfs/')
+    // return ipfsUri.replace('ipfs://', 'https://ipfs.jpgstoreapis.com/')
+    return ipfsUri.replace('ipfs://', 'https://ipfs.blockfrost.dev/ipfs/')
     // return `${ipfsUri.replace('ipfs://', 'https://')}.ipfs.nftstorage.link`
     // return ipfsUri.replace('ipfs://', 'https://alwaysinvert.mypinata.cloud/ipfs/')
   }
