60ddcca8a673ca276682335c1b38bf2c74e9647e
Ben Elferink <ben.elferink@icloud.com>
2023-03-14 22:21:49 +0200

    ui tweak

--- pages/tokens/index.tsx
+++ pages/tokens/index.tsx
@@ -9,7 +9,7 @@ const Page = () => {
         {navTokens.map(({ label, path }) =>
           path ? (
             <Link key={`token_${label}`} scroll={false} href={path}>
-              <div className='relative w-44 h-44 m-2 flex flex-col justify-center rounded-xl bg-gray-400 hover:bg-gray-200 bg-opacity-20 hover:bg-opacity-20 hover:text-gray-200'>
+              <div className='w-44 h-44 m-2 py-6 flex flex-col justify-between rounded-xl bg-gray-400 hover:bg-gray-200 bg-opacity-20 hover:bg-opacity-20 hover:text-gray-200'>
                 <div className='flex items-center justify-center animate-pulse'>
                   <ImageLoader
                     src={`/media/tokens/${label.toLowerCase()}/token.png`}
@@ -18,7 +18,7 @@ const Page = () => {
                     height={label === 'CSWAP' ? 80 : 100}
                   />
                 </div>
-                <h3 className='absolute bottom-1 left-1/2 -translate-x-1/2 text-center text-sm'>${label}</h3>
+                <h3 className='text-center text-sm'>${label}</h3>
               </div>
             </Link>
           ) : null
