230104814d87b886f751cf1eadaa5302d3ce1945
Ben Elferink <ben.elferink@icloud.com>
2023-03-27 12:02:41 +0300

    C4 staking is live

--- pages/tokens/c4.tsx
+++ pages/tokens/c4.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import TokenHeader from '../../components/tokens/TokenHeader'
 import TokenPrerequisites from '../../components/tokens/TokenPrerequisites'
 import TokenWhoEarns, { WhoCanEarn } from '../../components/tokens/TokenWhoEarns'
@@ -24,15 +25,42 @@ const Page = () => {
         items={[
           {
             imageUrl: '/media/tokens/c4/living-land.png',
-            purchaseUrl: 'https://swamplands.cardanocrocsclub.com/swamp',
-            texts: ['CCC - Living Land'],
+            purchaseUrl: 'https://swamplands.cardanocrocsclub.com/',
+            texts: ['CCC - Living Land', '1 = stake 1 NFT(s)'],
           },
         ]}
       />
 
       <div className='w-full my-2 p-4 px-6 flex flex-col bg-gray-400 bg-opacity-20 rounded-xl'>
         <h4 className='mb-2 text-gray-200 text-lg text-center'>How to earn?</h4>
-        <p className='mx-auto text-sm'>Coming soon.</p>
+
+        <ol className='mx-auto list-decimal list-inside'>
+          <li className='text-sm'>
+            Connect your wallet to the{' '}
+            <Link
+              href='https://swamplands.cardanocrocsclub.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-400'
+            >
+              CCC Swamp
+            </Link>
+            .
+          </li>
+          <li className='text-sm'>
+            Navigate to the{' '}
+            <Link
+              href='https://swamplands.cardanocrocsclub.com/swamp-lands-aplications'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-400'
+            >
+              Swamplands Apps
+            </Link>{' '}
+            tab.
+          </li>
+          <li className='text-sm'>Select your Living Lands and stake your NFTs.</li>
+        </ol>
       </div>
 
       <div className='w-full my-2 p-4 px-6 bg-gray-400 bg-opacity-20 rounded-xl'>
@@ -40,7 +68,15 @@ const Page = () => {
 
         <div className='mx-auto w-fit'>
           <p className='text-sm text-center'>
-            300 ${TOKEN_NAME} per 1 (eligible) NFT,
+            Level 1: 300 ${TOKEN_NAME} per 1 (eligible) NFT,
+            <br />
+            Level 2: 330 ${TOKEN_NAME} per 1 (eligible) NFT,
+            <br />
+            Level 3: 360 ${TOKEN_NAME} per 1 (eligible) NFT,
+            <br />
+            Level 4: 390 ${TOKEN_NAME} per 1 (eligible) NFT,
+            <br />
+            Level 5: 420 ${TOKEN_NAME} per 1 (eligible) NFT,
             <br />
             every month.
           </p>
