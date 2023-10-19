f9f3afa4dee242d5a4df11981e8210758d12362a
Ben Elferink <ben.elferink@icloud.com>
2023-10-19 18:08:36 +0300

    fix ipfs

--- functions/formatters/formatIpfsUrl.ts
+++ functions/formatters/formatIpfsUrl.ts
@@ -1,7 +1,7 @@
 const formatIpfsUrl = (ipfsUri: string) => {
-  return ipfsUri.replace('ipfs://', 'https://ipfs.blockfrost.dev/ipfs/')
-
-  // return ipfsUri.replace('ipfs://', 'https://image-optimizer.jpgstoreapis.com/')
+  return ipfsUri.replace('ipfs://', 'https://image-optimizer.jpgstoreapis.com/')
+  
+  // return ipfsUri.replace('ipfs://', 'https://ipfs.blockfrost.dev/ipfs/')
   // return ipfsUri.replace('ipfs://', 'https://ipfs.jpgstoreapis.com/')
   // return ipfsUri.replace('ipfs://', 'https://images.cnft.tools/ipfs/')
   // return ipfsUri.replace('ipfs://', 'https://nftstorage.link/ipfs/')
