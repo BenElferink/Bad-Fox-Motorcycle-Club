303fd439afa6242f8eecc0f7abb7a58db8435d6f
Ben Elferink <ben.elferink@icloud.com>
2024-11-25 16:13:25 +0200

    fix: trade-sync

--- src/pages/api/trade/sync.ts
+++ src/pages/api/trade/sync.ts
@@ -1,45 +1,47 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
-import { firestore } from '@/src/utils/firebase';
-import type { Trade } from '@/src/@types';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+import { firestore } from '@/src/utils/firebase'
+import type { Trade } from '@/src/@types'
+import { IS_DEV } from '@/src/constants'
 
 export const config = {
   maxDuration: 300,
   api: {
     responseLimit: false,
   },
-};
+}
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method } = req;
+  const { method } = req
 
   try {
     switch (method) {
       case 'GET': {
-        const collection = firestore.collection('trades');
-        const { docs } = await collection.where('withdrawTx', '==', '').get();
+        const collection = firestore.collection('trades')
+        const { docs } = await collection.where('withdrawTx', '==', '').get()
 
-        const now = Date.now();
+        const now = Date.now()
         const needToWithdraw = docs
           .map((doc) => ({ ...(doc.data() as Trade), id: doc.id }))
-          .filter(({ timestamp }) => timestamp && now - timestamp > 3 * 60 * 1000);
+          .filter(({ timestamp }) => timestamp && now - timestamp > 3 * 60 * 1000)
 
-        for await (const { id } of needToWithdraw) {
-          await axios.post('/api/trade', { docId: id });
-        }
+        // for await (const { id } of needToWithdraw) {
+        const { id } = needToWithdraw[0] || {}
+        if (!!id) await axios.post(IS_DEV ? 'http://localhost:3000' : 'https://badfoxmc.com' + '/api/trade', { docId: id })
+        // }
 
-        return res.status(204).end();
+        return res.status(200).json({ needToWithdraw })
       }
 
       default: {
-        res.setHeader('Allow', 'GET');
-        return res.status(405).end();
+        res.setHeader('Allow', 'GET')
+        return res.status(405).end()
       }
     }
   } catch (error) {
-    console.error(error);
-    return res.status(500).end();
+    console.error(error)
+    return res.status(500).end()
   }
-};
+}
 
-export default handler;
+export default handler
